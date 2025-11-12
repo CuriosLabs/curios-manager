7b18000d89c266c120a2facd25e83e57ff94cae2
VideoCurio <david@videocurio.com>
2025-11-12 16:51:06 +0100

    Updated as a pkgs fetch from Github.

--- README.md
+++ README.md
@@ -4,3 +4,22 @@
 A modern TUI to control your [CuriOS](https://github.com/CuriosLabs/CuriOS) system.
 
 ![CuriOs Manager TUI](https://github.com/CuriosLabs/CuriOS/blob/testing/img/CuriOS-manager.png?raw=true "CuriOS manager")
+
+-----
+
+## License
+
+Copyright (C) 2025  David BASTIEN
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
--- default.nix
+++ default.nix
@@ -1,51 +1,5 @@
-# CuriOS Manager package.
-# Various tools to manage your CuriOS system.
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.callPackage ./pkgs/curios-manager {}
 
-{ lib, stdenvNoCC, pkgs }:
-stdenvNoCC.mkDerivation rec {
-  pname = "curios-manager";
-  version = "0.7";
-
-  src = lib.fileset.toSource {
-    root = ./.;
-    fileset = lib.fileset.unions [
-      ./bin
-      ./share
-    ];
-  };
-
-  dontPatch = false;
-  dontConfigure = true;
-  dontBuild = true;
-  postPatch = ''
-    patchShebangs
-  '';
-  desktopItem = pkgs.makeDesktopItem {
-    name = "dev.curioslabs.curios.manager";
-    exec = "/run/current-system/sw/bin/alacritty -e curios-manager";
-    desktopName = "CuriOS Manager CLI";
-    icon = "desktop-curios-manager";
-    categories = [ "System" ];
-    terminal = true;
-  };
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p  $out/bin/
-    install -D -m 555 -t $out/bin/ bin/curios-manager
-
-    mkdir -p $out/share
-    cp -r ${desktopItem}/share/applications $out/share
-    mkdir -p $out/share/icons/hicolor/scalable/apps
-    cp share/icons/hicolor/scalable/apps/nixos.svg $out/share/icons/hicolor/scalable/apps/desktop-curios-manager.svg
-
-    runHook postInstall
-  '';
-
-  meta = {
-    description = "CuriOS manager";
-    homepage = "https://github.com/CuriosLabs/CuriOS";
-    license = lib.licenses.gpl3Only;
-    platforms = lib.platforms.linux;
-  };
-}
+# test it locally with:
+# nix-build && nix-env -i -f default.nix
