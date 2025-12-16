15d0159c6b2d60023bedfb4bd310c5f824578bb0
VideoCurio <david@videocurio.com>
2025-12-16 17:14:11 +0100

    Fix help_menu loop

--- pkgs/curios-manager/bin/functions/help_menu.sh
+++ pkgs/curios-manager/bin/functions/help_menu.sh
@@ -7,35 +7,40 @@ help_menu() {
   case $HELP_MENU in
   " Shortcuts")
     cosmic-settings keyboard 2>/dev/null
+    help_menu
     ;;
   "󰄄 CuriOS")
     if ! xdg-open "https://github.com/CuriosLabs/CuriOS"; then
       echo -e "${RED}Failed to open URL. Is a web browser installed?${NC}"
     fi
+    help_menu
     ;;
   "󰄄 CuriOS - report a bug")
     if ! xdg-open "https://github.com/CuriosLabs/CuriOS/issues"; then
       echo -e "${RED}Failed to open URL. Is a web browser installed?${NC}"
     fi
+    help_menu
     ;;
   " NixOS Wiki")
     if ! xdg-open "https://wiki.nixos.org/wiki/NixOS_Wiki"; then
       echo -e "${RED}Failed to open URL. Is a web browser installed?${NC}"
     fi
+    help_menu
     ;;
   " NixOS forum")
     if ! xdg-open "https://discourse.nixos.org/"; then
       echo -e "${RED}Failed to open URL. Is a web browser installed?${NC}"
     fi
+    help_menu
     ;;
   " NixOS manual")
     if ! xdg-open "https://nixos.org/manual/nixos/stable/"; then
       echo -e "${RED}Failed to open URL. Is a web browser installed?${NC}"
     fi
+    help_menu
     ;;
   " Back")
     main_menu
     ;;
   esac
 }
-
