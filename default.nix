51b28b7f4b8aadd05da0badbdd9c46ec41ed4582
VideoCurio <david@videocurio.com>
2026-01-14 14:34:04 +0100

    Added nix profile instructions

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 pkgs.callPackage ./pkgs/curios-manager {}
 
 # test it locally with:
-# nix-build && nix-env -i -f default.nix
+# nix-build && nix profile add -f default.nix
 # See:
 #nix profile list
-#nix-env --uninstall curios-manager
+#nix profile remove curios-manager
