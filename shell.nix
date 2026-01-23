014ffbc3b5999b5784397f7b99e527ec0d684181
VideoCurio <david@videocurio.com>
2026-01-23 16:15:43 +0100

    Added skeleton for COSMIC applet

---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell rec {
+  nativeBuildInputs = with pkgs; [ pkg-config just ];
+
+  buildInputs = with pkgs; [
+    rustup
+    cargo-c
+
+    # Runtime dependencies which need to be in LD_LIBRARY_PATH
+    wayland
+    libxkbcommon
+    vulkan-loader
+    libGL
+
+    # X11 fallback deps (often checked by winit)
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXrandr
+  ];
+
+  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+}
+
