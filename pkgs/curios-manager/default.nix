7aad09ba021c4190bec728b61da482ca6302f18a
VideoCurio <david@videocurio.com>
2026-02-19 10:07:22 +0100

    Release 0.30.3

--- pkgs/curios-manager/default.nix
+++ pkgs/curios-manager/default.nix
@@ -4,13 +4,13 @@
 { lib, stdenvNoCC, fetchFromGitHub, pkgs, makeWrapper }:
 stdenvNoCC.mkDerivation rec {
   pname = "curios-manager";
-  version = "0.30.2";
+  version = "0.30.3";
 
   src = fetchFromGitHub {
     owner = "CuriosLabs";
     repo = "curios-manager";
     rev = version;
-    hash = "sha256-WBK+0bitHo2GAs1uw63+MIDPHLjaL1JbKofMqNItFVs=";
+    hash = "";
   };
 
   buildInputs = [
